cab65109bbf5208a8ad6f7940f391fe9fe194efb
jorisvanduyse <jorisduyse@gmail.com>
2024-03-24 19:28:21 +0100

    v0.1.0 - Basic WebGPU vertex shader

---
+++ README.md
@@ -0,0 +1,5 @@
+# Game Oof Life
+
+## WebGPU running Conway's Game of Life
+This implementation was based on the Google codelab "[Your first WebGPU app](https://codelabs.developers.google.com/your-first-webgpu-app)"
+This proof of concept code is available at [gool.qwict.com](https://gool.qwict.com).
---
+++ gool_vertex_shader.wgsl.js
@@ -0,0 +1,48 @@
+// Install WGSL Literal on vscode to get SYNTAX highlights in this code :)
+export const shader = /* wgsl */`
+struct VertexInput {
+  @location(0) pos: vec2f,
+  @builtin(instance_index) instance: u32,
+};
+
+struct VertexOutput {
+  @builtin(position) pos: vec4f,
+  @location(0) cell: vec2f,
+};
+
+@group(0) @binding(0) var<uniform> grid: vec2f;
+
+@group(0) @binding(1) var<storage> cellState: array<u32>; // New!
+
+
+@vertex
+fn vertexMain(input: VertexInput) -> VertexOutput  {
+  let i = f32(input.instance);
+  let cell = vec2f(i % grid.x, floor(i / grid.x));
+  let state = f32(cellState[input.instance]); // New line!
+
+  let cellOffset = cell / grid * 2;
+  let gridPos = (input.pos*state+1) / grid - 1 + cellOffset;
+  
+  var output: VertexOutput;
+  output.pos = vec4f(gridPos, 0, 1);
+  output.cell = cell;
+  return output;
+
+}
+
+struct FragInput {
+  @location(0) cell: vec2f,
+};
+
+
+@fragment
+fn fragmentMain(input: FragInput) -> @location(0) vec4f {
+  let max = grid / 2;
+  let closeX = abs(input.cell.x - max);
+  let closeY = abs(input.cell.y - max);
+  let closeToCenter = 1 - (closeX + closeY) / (0.5 * grid);
+  return vec4f(closeToCenter - 0.5, closeToCenter.x, 1);
+}
+
+`;
